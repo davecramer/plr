dd50a24e73b85e13b0a43d6ff30ae3d33e8bf2b3
Dave Cramer <davecramer@gmail.com>
2017-11-06 15:09:35 -0500

    fix spelling of psql

--- testplr.bat
+++ testplr.bat
@@ -7,7 +7,7 @@ bin\initdb -D data
 bin\pg_ctl -D data -l logfile start
 popd
 pushd c:\projects\postgresql\src\pl\plr
-cp plr.control c:\psql\share\extension
+cp plr.control c:\pgsql\share\extension
 cp plr--8.3.0.17.sql c:\pgsql\share\extension
 popd
 vcregress plcheck > regression.out
