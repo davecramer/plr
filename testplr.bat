5412a9dc15db01b66e4a5da0d630a90d9c6a8a64
Dave Cramer <davecramer@gmail.com>
2017-11-06 14:35:00 -0500

    use perl to call instal

--- testplr.bat
+++ testplr.bat
@@ -1,7 +1,7 @@
 SET R_HOME=C:\R-3.2.4
 SET PATH=%PATH%;C:\R-3.2.4\bin\x64
 cd c:\projects\postgresql\src\tools\msvc
-install c:\pgsql
+perl install c:\pgsql
 pushd c:\pgsql
 bin\initdb -D data
 bin\pg_ctl -D data -l logfile start
