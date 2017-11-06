f0af93d2f8b82a54c4befd13b04a148390c661b5
Dave Cramer <davecramer@gmail.com>
2017-11-06 13:55:48 -0500

    use batchfile to run tests

---
+++ testplr.bat
@@ -0,0 +1,13 @@
+SET R_HOME=C:\R-3.2.4
+SET PATH=%PATH%;C:\R-3.2.4\bin\x64
+install c:\pgsql
+pushd c:\pgsql
+bin\initdb -D data
+bin\pg_ctl -D data -l logfile start
+popd
+pushd c:\projects\postgresql\src\pl\plr
+cp plr.control c:\psql\share\extension
+cp plr--8.3.0.17.sql c:\pgsql\share\extension
+popd
+vcregress plcheck > regression.out
+type regression.out
