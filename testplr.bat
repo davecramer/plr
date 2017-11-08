9ced561d066782732865d126039c1987085fdca2
Dave Cramer <davecramer@gmail.com>
2017-11-08 08:43:24 -0500

    call pg_regress directly

--- testplr.bat
+++ testplr.bat
@@ -8,6 +8,6 @@ pushd c:\projects\postgresql\src\pl\plr
 cp plr.control c:\pgsql\share\extension
 cp plr--8.3.0.17.sql c:\pgsql\share\extension
 popd
-perl vcregress.pl plcheck > regression.out
+../../../Release/pg_regress/pg_regress --bindir=../../../Release/psql --dbname=pl_regression plr > regression.out
 type regression.out
 
